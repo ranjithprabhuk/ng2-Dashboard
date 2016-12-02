809f051bab5f46e151fdfb327bf17754e2a91a2d
ranjithprabhuk <ranjithprabhu.k@gmail.com>
2016-12-02 18:46:40 +0530

    high chart components are added

--- src/modules/dashboard/components/chart/highchart.component.ts
+++ src/modules/dashboard/components/chart/highchart.component.ts
@@ -1,19 +1,40 @@
 import { Component } from '@angular/core';
+import { ChartService } from './chart.service';
 
 @Component({
     selector: 'high-chart',
     templateUrl: './view/high-chart.html',
-    styleUrls: ['./styles/high-chart.css']
+    styleUrls: ['./styles/high-chart.css'],
+    providers:[ChartService]
 })
 
 export class HighChartComponent {
-    public options: Object;
-    constructor() {
-        this.options = {
-            title: { text: 'simple chart' },
-            series: [{
-                data: [29.9, 71.5, 106.4, 129],
-            }]
-        };
+
+    public simpleLineChart: Object;
+    public stockChart: Object;
+    constructor(private chartService:ChartService) { }
+
+    //get chart simple chart data
+    public _getLineChartData():void {
+        this.chartService.getChartData('highchart/simple-line-chart.json').then(res =>{
+            console.log("response>>",res);
+        }).catch(err=>{console.log("response>>",err); });
     }
+
+    //get chart stock chart data
+    public _getStockChartData():void {
+        this.chartService.getChartData('highchart/stock-chart.json').then(res =>{
+            console.log("response11>>",res);
+        }).catch(err=> err);
+    }
+
+
+
+    OnNgInit() {
+
+        //call the api on page load
+        this._getLineChartData();
+        this._getStockChartData();
+    }
+
 }
