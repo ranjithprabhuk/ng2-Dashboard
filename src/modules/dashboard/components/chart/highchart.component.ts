7d60c2aea2f0a8ec38a1099fb724f33a43e34015
ranjithprabhuk <ranjithprabhu.k@gmail.com>
2016-12-02 18:52:44 +0530

    two charts are upadted in high charty

--- src/modules/dashboard/components/chart/highchart.component.ts
+++ src/modules/dashboard/components/chart/highchart.component.ts
@@ -5,32 +5,44 @@ import { ChartService } from './chart.service';
     selector: 'high-chart',
     templateUrl: './view/high-chart.html',
     styleUrls: ['./styles/high-chart.css'],
-    providers:[ChartService]
+    providers: [ChartService]
 })
 
 export class HighChartComponent {
 
     public simpleLineChart: Object;
     public stockChart: Object;
-    constructor(private chartService:ChartService) { }
+    constructor(private chartService: ChartService) { }
 
     //get chart simple chart data
-    public _getLineChartData():void {
-        this.chartService.getChartData('highchart/simple-line-chart.json').then(res =>{
-            console.log("response>>",res);
-        }).catch(err=>{console.log("response>>",err); });
+    public _getLineChartData(): void {
+        this.chartService.getChartData('highchart/simple-line-chart.json').then(res => {
+            if (res)
+                this.simpleLineChart = res;
+        });
     }
 
     //get chart stock chart data
-    public _getStockChartData():void {
-        this.chartService.getChartData('highchart/stock-chart.json').then(res =>{
-            console.log("response11>>",res);
-        }).catch(err=> err);
+    public _getStockChartData(): void {
+        this.chartService.getChartData('highchart/stock-chart.json').then(res => {
+            if (res) {
+                this.stockChart = {
+                    title: { text: 'AAPL Stock Price' },
+                    series: [{
+                        name: 'AAPL',
+                        data: res,
+                        tooltip: {
+                            valueDecimals: 2
+                        }
+                    }]
+                };
+            }
+        });
     }
 
 
 
-    OnNgInit() {
+    ngOnInit() {
 
         //call the api on page load
         this._getLineChartData();
