a9d01d611fc04f797c645eff4c304361938a6f12
ranjithprabhuk <ranjithprabhu.k@gmail.com>
2016-12-06 17:13:34 +0530

    calendar components are added

--- src/modules/dashboard/components/calendar/calendar.component.ts
+++ src/modules/dashboard/components/calendar/calendar.component.ts
@@ -1,4 +1,38 @@
 import { Component } from '@angular/core';
+import {
+  startOfDay,
+  subDays,
+  addDays,
+  endOfMonth,
+  isSameDay,
+  isSameMonth,
+  addWeeks,
+  subWeeks,
+  addMonths,
+  subMonths,
+  addHours
+} from 'date-fns';
+import { Subject } from 'rxjs/Subject';
+
+import {
+  CalendarEvent,
+  CalendarEventAction
+} from 'angular2-calendar';
+
+const colors: any = {
+  red: {
+    primary: '#ad2121',
+    secondary: '#FAE3E3'
+  },
+  blue: {
+    primary: '#1e90ff',
+    secondary: '#D1E8FF'
+  },
+  yellow: {
+    primary: '#e3bc08',
+    secondary: '#FDF1BA'
+  }
+};
 
 @Component({
     selector: 'event-calendar',
@@ -6,7 +40,99 @@ import { Component } from '@angular/core';
     styleUrls:['./styles/angular2-calendar.css']
 })
 
+
+
 export class CalendarComponent {
-    viewDate: Date = new Date();
-    events = [];
+    view: string = 'month';
+
+  viewDate: Date = new Date();
+
+  actions: CalendarEventAction[] = [{
+    label: '<i class="fa fa-fw fa-pencil"></i>',
+    onClick: ({event}: {event: CalendarEvent}): void => {
+      console.log('Edit event', event);
+    }
+  }, {
+    label: '<i class="fa fa-fw fa-times"></i>',
+    onClick: ({event}: {event: CalendarEvent}): void => {
+      this.events = this.events.filter(iEvent => iEvent !== event);
+    }
+  }];
+
+  refresh: Subject<any> = new Subject();
+
+  events: CalendarEvent[] = [{
+    start: subDays(startOfDay(new Date()), 1),
+    end: addDays(new Date(), 1),
+    title: 'A 3 day event',
+    color: colors.red,
+    actions: this.actions
+  }, {
+    start: startOfDay(new Date()),
+    title: 'An event with no end date',
+    color: colors.yellow,
+    actions: this.actions
+  }, {
+    start: subDays(endOfMonth(new Date()), 3),
+    end: addDays(endOfMonth(new Date()), 3),
+    title: 'A long event that spans 2 months',
+    color: colors.blue
+  }, {
+    start: addHours(startOfDay(new Date()), 2),
+    end: new Date(),
+    title: 'A resizable event',
+    color: colors.yellow,
+    actions: this.actions
+  }];
+
+  activeDayIsOpen: boolean = true;
+
+  increment(): void {
+
+    const addFn: any = {
+      day: addDays,
+      week: addWeeks,
+      month: addMonths
+    }[this.view];
+
+    this.viewDate = addFn(this.viewDate, 1);
+
+  }
+
+  decrement(): void {
+
+    const subFn: any = {
+      day: subDays,
+      week: subWeeks,
+      month: subMonths
+    }[this.view];
+
+    this.viewDate = subFn(this.viewDate, 1);
+
+  }
+
+  today(): void {
+    this.viewDate = new Date();
+  }
+
+  dayClicked({date, events}: {date: Date, events: CalendarEvent[]}): void {
+
+    if (isSameMonth(date, this.viewDate)) {
+      if (
+        (isSameDay(this.viewDate, date) && this.activeDayIsOpen === true) ||
+        events.length === 0
+      ) {
+        this.activeDayIsOpen = false;
+      } else {
+        this.activeDayIsOpen = true;
+        this.viewDate = date;
+      }
+    }
+  }
+
+  eventTimesChanged({event, newStart, newEnd}: any): void {
+    event.start = newStart;
+    event.end = newEnd;
+    this.refresh.next();
+  }
 }
